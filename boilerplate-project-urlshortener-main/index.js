d2f4576d70ec726078de4277305286e9faf1861d
end garv <skydorw@gmail.com>
2024-02-20 17:32:22 +0200

    [boilerplate-project-urlshortener-main] Successfully completed test!

--- boilerplate-project-urlshortener-main/index.js
+++ boilerplate-project-urlshortener-main/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const ShortURL = require('./schema.js').URLModel;
+const dns = require('node:dns')
 mongoose.connect(process.env.MONGO_URI, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
@@ -21,14 +22,44 @@ app.get('/', function (req, res) {
 	res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.get('/api/shorturl/:id', (req, res) => {
+app.get('/api/shorturl/:id', async (req, res) => {
     const {id} = req.params
-    res.redirect('https://www.freecodecamp.org')
+    console.log(id, typeof id)
+    try {
+        const shortenedURL = await ShortURL.findOne({shortURL: Number(id)})
+        if(shortenedURL) {
+            return res.redirect(shortenedURL.originalURL)
+        }
+        console.log('Error', shortenedURL)
+    } catch (err) {
+    }
 })
 
-app.post('/api/shorturl', (req, res) => {
+app.post('/api/shorturl', async (req, res) => {
     const {url} = req.body
-    res.json({originalUrl: url, short: 1})
+    const isInvalidURL = !/^http(s|):\/\/(w{3}\.|)[a-zA-Z0-9-_]+(\.\w{2,3}|:\d{4})(\/\w+|)/.test(url)
+    if(isInvalidURL) {
+        console.log(url)
+        res.json({error: 'invalid url'})
+        return 
+    }
+
+    try {
+        const existingURL = await ShortURL.findOne({ originalURL: url });
+
+        if (existingURL) {
+            return res.json({ original_url: url, short_url: existingURL.shortURL });
+        }
+
+        const documentCount = await ShortURL.estimatedDocumentCount()
+        new ShortURL({
+            originalURL: url,
+            shortURL: documentCount + 1
+        }).save()
+        return res.json({ original_url: url, short_url: documentCount + 1 })
+    } catch(err) {
+        throw new Error(`Somthing went wrong: ${err}`)
+    }
 })
 
 app.listen(port, function () {
--- boilerplate-project-urlshortener-main/schema.js
+++ boilerplate-project-urlshortener-main/schema.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose')
 const shortURL = new mongoose.Schema({
-    id: {
+    shortURL: {
         type: Number,
-        required: true,
-        unique: true
+        required: true
     },
     originalURL: {
         type: String,
